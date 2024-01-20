e35a428d2a03e5d96de488e421e610efe97aaed1
gencyigitcan <yigitcangenc@gmail.com>
2024-01-20 18:31:50 +0300

    Label ve Textbox Kullanımı

--- Arac_Kullanimi/Form1.cs
+++ Arac_Kullanimi/Form1.cs
@@ -21,5 +21,29 @@ namespace Arac_Kullanimi
         {
             MessageBox.Show("Merhaba Dünya!");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Yiğitcan";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Şimdilik boş duruyor.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label5.Text = "Yiğitcan";
+            label7.Text = "Genç";
+            label9.Text = "Full Stack Developer";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // textBox1.Text = "Full Stack Developer";
+
+            label2.Text = textBox1.Text;
+        }
     }
 }
